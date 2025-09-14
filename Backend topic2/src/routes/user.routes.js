08755da060b19355c4359e94609bcd0d52b99b68
Manav Khare <manavkhare121@gmail.com>
2025-09-14 11:13:20 +0530

    Add new routes for usercontrollels

--- Backend topic2/src/routes/user.routes.js
+++ Backend topic2/src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import { loginuser, logoutuser, registerUser } from "../controllers/user.controllers.js";
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserCoverImage,
+  updateUserAvatar,
+  loginuser,
+  logoutuser,
+  registerUser,
+  getUserChannelProfile,
+  getWatchhistory,
+} from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { refreshAccessToken } from "../controllers/user.controllers.js";
@@ -13,8 +24,20 @@ router.route("/register").post(
   registerUser
 );
 
-router.route("/login").post(loginuser)
+router.route("/login").post(loginuser);
 //secured routes
-router.route("/logout").post(verifyJWT,logoutuser)
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/logout").post(verifyJWT, logoutuser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("/coverimage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile); //url that is why get http is using
+router.route("/history").get(verifyJWT, getWatchhistory);
+
 export default router;
